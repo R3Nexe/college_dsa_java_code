1b17b324502a1ab5f1ac3e7e32cf7ee31f082221
R3Nexe <nishhu24@gmail.com>
2025-05-04 20:56:19 +0530

    Update a1hq2.java

--- DSA/Assignment_1/a1hq2.java
+++ DSA/Assignment_1/a1hq2.java
@@ -1,21 +1,12 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-
-public class ReverseLines {
+public class a1hq2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> lines = new ArrayList<>();
-
-        // Read all lines
-        while (scanner.hasNextLine()) {
-            lines.add(scanner.nextLine());
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the number of lines to input");
+        int n=sc.nextInt();
+        String ar[]= new String[n];
+        for (int i = 0; i < ar.length; i++) {
+            ar[i]=sc.nextLine();
         }
-
-        // Print lines in reverse order
-        for (int i = lines.size() - 1; i >= 0; i--) {
-            System.out.println(lines.get(i));
-        }
-
-        scanner.close();
-    }
-}
+        for (int i = ar.length-1; i >= 0; i++) {
+            System.out.println(ar[i]);
