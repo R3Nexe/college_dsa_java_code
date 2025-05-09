97738ec85f3e6bb009d9c5adb5e614b5d89267cb
R3Nexe <nishhu24@gmail.com>
2025-05-09 09:57:00 +0530

    Update a2q5.java

--- DSA/Assignment_2/a2q5.java
+++ DSA/Assignment_2/a2q5.java
@@ -1,30 +1,60 @@
-
-class Person{
-    String name;
-    int age;
-    
+// Abstract base class
+abstract class Shape {
+    abstract double getArea();
 }
-class Employee extends Person{
-    int Eid;
-    int salary;
-    Employee(String name, int age,int Eid, int salary){
-        this.name=name;
-        this.age=age;
-        this.Eid=Eid;
-        this.salary=salary;
+
+// Square class
+class Square extends Shape {
+    private double side;
+
+    Square(double side) {
+        this.side = side;
     }
-    void empDisplay(){
-        System.out.println("name: "+name);
-        System.out.println("age: "+age);
-        System.out.println("Employee Id: "+Eid);
-        System.out.println("Employee salary: "+salary);
+
+    @Override
+    double getArea() {
+        return side * side;
     }
 }
 
-public class a2q5 {
+// Triangle class
+class Triangle extends Shape {
+    private double base, height;
+
+    Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    double getArea() {
+        return 0.5 * base * height;
+    }
+}
+
+// Circle class
+class Circle extends Shape {
+    private double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+// Main class
+public class ShapeAreaDemo {
     public static void main(String[] args) {
-        Employee e1=new Employee("nishant", 20, 1211, 3500000);
-        e1.empDisplay();
+        Shape square = new Square(5);
+        Shape triangle = new Triangle(4, 6);
+        Shape circle = new Circle(3);
+
+        System.out.println("Square area: " + square.getArea());
+        System.out.println("Triangle area: " + triangle.getArea());
+        System.out.println("Circle area: " + circle.getArea());
     }
 }
- 
